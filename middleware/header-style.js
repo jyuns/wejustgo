d06a69f53f716dd08030d3290cc24d847ceb7fc6
Yunknows <jyuns@kakao.com>
2019-12-19 00:51:35 +0900

    move repository

--- middleware/header-style.js
+++
@@ -1,9 +0,0 @@
-export default function(context) {
-
-    if(context.route.path != '/' ) {
-        context.store.commit('SET_HEADER_STYLE', false)
-    } else {
-        context.store.commit('SET_HEADER_STYLE', true)
-    }
-
-}
